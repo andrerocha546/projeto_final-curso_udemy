d412897f818f5effd6461ef994c4349e9cf50ac4
andrerocha546 <41653025+andrerocha546@users.noreply.github.com>
2020-09-25 17:54:02 -0300

    adicionando cards na home que mostram as estatisticas trazidas do backend

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -9,6 +9,9 @@ import router from './config/router'
 
 Vue.config.productionTip = false
 
+// TEMPORARIO
+require('axios').defaults.headers.common['Authorization'] = 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwibmFtZSI6IkFuZHJlIFMuIFJvY2hhIiwiZW1haWwiOiJhbmRyZXJvY2hhNTQ2QGdtYWlsLmNvbSIsImFkbWluIjp0cnVlLCJpYXQiOjE2MDEwNjU1NTYsImV4cCI6MTYwMTMyNDc1Nn0.AOEuQCfoVQPxtL61yFoYuQldmA61rETrAYJW7DEQkZg'
+
 new Vue({
   store,
   router,
