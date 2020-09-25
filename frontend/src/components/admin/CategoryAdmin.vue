eee99b0b465394e4fccee062e8f0842ed1678c94
andrerocha546 <41653025+andrerocha546@users.noreply.github.com>
2020-09-25 18:17:36 -0300

    formando parte da tela de admin que tem as abas artigos, categorias e usuários

---
+++ frontend/src/components/admin/CategoryAdmin.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="category-admin">
+        categoria componente
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'CategoryAdmin'
+}
+</script>
+
+<style>
+
+</style>
---
+++ frontend/src/components/admin/UserAdmin.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="user-admin">
+        usuario componente
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'UserAdmin'
+}
+</script>
+
+<style>
+
+</style>
