fe1b72386b9a6852979a527f47d5d23494950fbf
andrerocha546 <41653025+andrerocha546@users.noreply.github.com>
2020-09-28 14:25:10 -0300

    configurando Toasted para mostrar mensagens de sucesso ou erro

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,6 +4,7 @@ import Vue from 'vue'
 import App from './App'
 
 import './config/bootstrap'
+import './config/msgs'
 import store from './config/store'
 import router from './config/router'
 
@@ -16,4 +17,4 @@ new Vue({
   store,
   router,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app') 
