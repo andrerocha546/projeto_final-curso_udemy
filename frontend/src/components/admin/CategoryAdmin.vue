89fcf46846594c830431e8e73dc648640a6fa2dc
andrerocha546 <41653025+andrerocha546@users.noreply.github.com>
2020-09-30 17:46:04 -0300

    simplicando template

--- frontend/src/components/admin/CategoryAdmin.vue
+++ frontend/src/components/admin/CategoryAdmin.vue
@@ -1,53 +1,31 @@
 <template>
     <div class="category-admin">
         <b-form>
-            <input type="hidden" v-model="category.id" id="category-id">
-            <b-row>
-                <b-col xs="12">
-                    <b-form-group label="Nome:" label-for="category-name">
-                        <b-form-input id="category-name" type="text" 
-                            v-model="category.name" required
-                            :readonly="mode === 'remove'"
-                            placeholder="Informe o Nome da Categoria"/>
-                    </b-form-group>
-                </b-col>  
-            </b-row>
-            <b-row v-if="mode === 'save'">
-                <b-col xs="12">
-                    <b-form-group v-model="selected" label="Categoria Pai:" 
-                        label-for="category-parentId">
-                        <b-form-select id="category-parentId" 
-                            :options="categories"
-                            :readonly="mode === 'remove'"
-                            v-model="category.parentId"></b-form-select>
-                    </b-form-group> 
-                </b-col>
-            </b-row>
-            <b-row v-else>
-                <b-col xs="12">
-                    <b-form-group v-model="selected" label="Categoria Pai:" 
-                        label-for="category-parentId">
-                        <b-form-select id="category-parentId" 
-                            :options="categories"
-                            readonly
-                            v-model="category.path"></b-form-select>
-                    </b-form-group> 
-                </b-col>
-            </b-row>
-            <b-row>
-                <b-col xs="12">
-                    <b-button variant='primary' v-if="mode === 'save'"
-                        @click="save">Salvar</b-button>
-                    <b-button variant='danger' v-if="mode === 'remove'"
-                        @click="save">Excluir</b-button>
-                    <b-button class="ml-2"
-                        @click="save">Cancelar</b-button>
-                </b-col>
-            </b-row>
+            <input id="category-id" type="hidden" v-model="category.id" />
+            <b-form-group label="Nome:" label-for="category-name">
+                <b-form-input id="category-name" type="text"
+                    v-model="category.name" required
+                    :readonly="mode === 'remove'"
+                    placeholder="Informe o Nome da Categoria..." />
+            </b-form-group>
+            <b-form-group label="Categoria Pai:" label-for="category-parentId">
+                <b-form-select v-if="mode === 'save'"
+                    id="category-parentId"
+                    :options="categories" v-model="category.parentId" />
+                <b-form-input v-else
+                    id="category-parentId" type="text"
+                    v-model="category.path"
+                    readonly />
+            </b-form-group>
+            <b-button variant="primary" v-if="mode === 'save'"
+                @click="save">Salvar</b-button>
+            <b-button variant="danger" v-if="mode === 'remove'"
+                @click="remove">Excluir</b-button>
+            <b-button class="ml-2" @click="reset">Cancelar</b-button>
         </b-form>
-        <br>
+        <hr>
         <b-table hover striped :items="categories" :fields="fields">
-            <template v-slot="actions" v-bind="data">
+            <template slot="actions" slot-scope="data">
                 <b-button variant="warning" @click="loadCategory(data.item)" class="mr-2">
                     <i class="fa fa-pencil"></i>
                 </b-button>
