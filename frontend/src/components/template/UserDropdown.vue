2a435057e61e4f223b651c04496fcee075ca2315
andrerocha546 <41653025+andrerocha546@users.noreply.github.com>
2020-10-03 17:44:46 -0300

    primeira parte da construção do logout. Excluindo usuário do localStorage

--- frontend/src/components/template/UserDropdown.vue
+++ frontend/src/components/template/UserDropdown.vue
@@ -9,7 +9,7 @@
         </div>
         <div class="user-dropdown-content">
             <router-link to="/admin"><i class="fa fa-cogs"></i> Administração</router-link>
-            <a href=""><i class="fa fa-sign-out"></i> Sair </a>
+            <a href @click.prevent="logout"><i class="fa fa-sign-out"></i> Sair </a>
         </div>
     </div>    
 </template>
@@ -17,11 +17,19 @@
 <script>
 import { mapState } from 'vuex'
 import Gravatar from 'vue-gravatar'
+import { userkey } from '@/global'
 
 export default {
     name: 'UserDropdown',
     components: { Gravatar },
-    computed: mapState(['user'])
+    computed: mapState(['user']),
+    methods: {
+        logout() {
+            localStorage.removeItem(userkey)
+            this.$store.commit('setUser', null)
+            this.$router.push({ name: 'auth' })
+        }
+    }
 }
 </script>
 
